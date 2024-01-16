d8878110d03647d023984136942e4de8ac2c3fff
Your Name <you@example.com>
2024-01-16 18:08:21 +0300

    mt

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col lg:flex-row lg:justify-center  p-5  h-screen w-screen ">
+    <main className="flex flex-col lg:flex-row lg:justify-center  p-5  h-fit w-screen lg:gap-x-[5rem] lg:mt-[3rem] ">
       <div className=" mt-[5rem]  ">
         <div className="flex lg:flex-col">
           <div className="text-[45px] lg:text-[60px] text-[#313E51] font-normal">
