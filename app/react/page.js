0770c333493282995e15fc83b6bf91b9d6c94b15
Your Name <you@example.com>
2024-01-18 20:54:42 +0300

    sa

---
+++ app/react/page.js
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar";
+import React from "react";
+
+const ReactQuestion = () => {
+  return (
+    <div>
+      <Navbar title="REACT" src="/images/react-svgrepo-com.svg" />
+      <div></div>
+    </div>
+  );
+};
+
+export default ReactQuestion;
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ src, title }) => {
   return (
     <div className="flex justify-between lg:justify-center lg:items-center  mt-10 ">
       <div className="flex justify-between lg:w-3/4 w-screen p-5 lg:p-0">
-        <div className="flex gap-x-1">
+        <div className="flex gap-x-2 items-center">
           <Image src={src} width={src ? 40 : 0} height={src ? 40 : 0} />
           <div className="font-bold cursor-pointer text-xl text-[#313E51]">
             {title}
