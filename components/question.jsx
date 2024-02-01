7581acdce2ee27015c4957e882b2e7a3ac37dcb2
Your Name <you@example.com>
2024-02-01 15:42:19 +0300

    map inside of inside

--- components/question.jsx
+++ components/question.jsx
@@ -20,14 +20,21 @@ const Question = ({ question }) => {
             <div className="border border-black w-[500px] rounded-full h-[15px]"></div>
           </div>
           <div>
-            <ul className="w-[500px] shadow-lg rounded-lg h-fit  flex flex-row items-center border bg-white cursor-pointer p-3">
-              <div className="ms-5  bg-[#F4F6FA]  text-[#626C7F] w-[40px] flex justify-center text-[28px] font-bold rounded-lg">
-                A
-              </div>
-              <div className="text-[28px] text-[black] font-bold w-[350px] ms-10  flex justify-center items-center  text-center">
-                React is a framework for JavaScript
-              </div>
-            </ul>
+            {quiz.questions[noq].options.map((option, index) => {
+              const letter = String.fromCharCode(65 + index);
+              return (
+                <ul>
+                  <li className="w-[500px] shadow-lg rounded-lg h-fit  flex flex-row items-center border bg-white cursor-pointer p-3">
+                    <div className="ms-5  bg-[#F4F6FA]  text-[#626C7F] w-[40px] flex justify-center text-[28px] font-bold rounded-lg">
+                      {letter}
+                    </div>
+                    <div className="text-[28px] text-[black] font-bold w-[350px] ms-10  flex justify-center items-center  text-center">
+                      {option}
+                    </div>
+                  </li>
+                </ul>
+              );
+            })}
             <button className="w-[500px] border rounded-lg bg-[#A729F5] h-[60px] mt-5 text-white text-[28px] font-bold">
               Submit
             </button>
