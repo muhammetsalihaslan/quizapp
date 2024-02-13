c7318eae7391ce8046b8bdb7456d012e6fc7dfc7
Your Name <you@example.com>
2024-02-13 18:08:23 +0300

    dark mode toggle

--- components/darkModeToggle.jsx
+++ components/darkModeToggle.jsx
@@ -1,13 +1,19 @@
+"use client";
+import { ThemeContext } from "@/context/ThemeContext";
 import Image from "next/image";
-import React from "react";
+import React, { useContext } from "react";
 
 const DarkModeToggle = () => {
+  const { toggle, mode } = useContext(ThemeContext);
   return (
-    <div className="flex gap-x-2">
+    <div className="flex gap-x-2" onClick={toggle}>
       <div>
         <Image src="/images/icon-sun-dark.svg" width={30} height={30} />
       </div>
-      <div className="border w-[60px] rounded-full bg-[#A729F5] flex items-center cursor-pointer">
+      <div
+        className="border w-[60px] rounded-full bg-[#A729F5] flex items-center cursor-pointer"
+        style={mode === "light" ? { left: "2px" } : { right: "2px" }}
+      >
         <div className="border bg-white  h-[20px] w-[20px] rounded-full ms-1"></div>
       </div>
       <div>
