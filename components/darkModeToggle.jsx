fded98fef94573583d2148cb1f28d93f32eae557
Your Name <you@example.com>
2024-02-15 14:19:12 +0300

    monn dark light

--- components/darkModeToggle.jsx
+++ components/darkModeToggle.jsx
@@ -11,7 +11,15 @@ const DarkModeToggle = () => {
   return (
     <div className="flex gap-x-2">
       <div>
-        <Image src="/images/icon-sun-dark.svg" width={30} height={30} />
+        <Image
+          src={
+            mode === "light"
+              ? "/images/icon-sun-dark.svg"
+              : "/images/icon-sun-light.svg"
+          }
+          width={30}
+          height={30}
+        />
       </div>
       <div
         className=" w-[60px] rounded-full bg-[#A729F5] flex items-center cursor-pointer"
@@ -22,7 +30,15 @@ const DarkModeToggle = () => {
         ></div>
       </div>
       <div>
-        <Image src="/images/icon-moon-dark.svg" width={30} height={30} />
+        <Image
+          src={
+            mode === "light"
+              ? "/images/icon-moon-dark.svg"
+              : "/images/icon-moon-light.svg"
+          }
+          width={30}
+          height={30}
+        />
       </div>
     </div>
   );
